ada27af516bdfb2da0cf077d5c252cfe55208a13
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-04 09:57:17 +0530

    Enhance LDR functionality with serial output and update platformio.ini for monitor settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
-#define ldrPin A0
+#define LDR_PIN A0
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    pinMode(LDR_PIN,INPUT);
+    Serial.println("LDR test");
+
 }
 
 void loop() {
 // write your code here
+    int ldrRaw = analogRead(LDR_PIN);
+    int lightPercent = map(ldrRaw,0,1023,100,0);
+    Serial.print("LDR Raw: ");
+    Serial.print(ldrRaw);
+    Serial.print(" | Light: ");
+    Serial.print(lightPercent);
+    Serial.println();
+    delay(500);
 }
